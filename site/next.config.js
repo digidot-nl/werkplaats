c1c3e13766e7028b2a20bf9445ccb6de185e36e2
digidot-nl <141425705+digidot-nl@users.noreply.github.com>
2025-02-09 19:45:06 +0100

    Update next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -2,15 +2,15 @@
 
 const nextConfig = {
   env: {
-    name: 'Kasm Technologies',
-    description: 'The official store for Kasm supported workspaces.',
+    name: 'MenM Inc.',
+    description: 'menm werkplaats',
     icon: '/img/logo.svg',
-    listUrl: 'https://registry.kasmweb.com/',
-    contactUrl: 'https://kasmweb.com/support',
+    listUrl: 'https://github.com/digidot-nl/werkplaats/',
+    contactUrl: 'https://github.com/digidot-nl/werkplaats/issues',
   },
   reactStrictMode: true,
   swcMinify: true,
-  basePath: '/kasm-registry/1.0',
+  basePath: '/werkplaats/1.0',
   trailingSlash: true,
   images: {
     unoptimized: true,
